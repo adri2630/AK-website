8a7e8d70ac35e3c5426a47ce264f9fa33928ebd1
adri2630 <adri2630@stud.kea.dk>
2020-11-17 16:52:14 +0100

    called categories and dont know whta to do with them lol

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 
 
+//fetch the categories
+fetch("http://keep-it-simple.site/wp-json/wp/v2/categories")
+.then(function(res){
+    console.log(res)
+    return res.json();
+})
+.then(function(datacat){
+        console.log(datacat)
+    receivedCat(datacat);
+})
+
+
+function receivedCat(elements){
+    console.log(elements)
+    elements.forEach(displayCategories)
+
+}
+// thi function will display one category find the template, clone it and fill it in and append it, what a multitasker
+function displayCategories(oneCat){
+        console.log(oneCat)
+    //fidn the template
+    const catTemplate = document.querySelector("#wordpresscategories").content;
+
+    //clone said template
+const catCopy = catTemplate.cloneNode(true);
+// fill it with the stuffypu put in wordpress for better or worse why can i just put here lorem ipsum o sth ?
+
+   catCopy.querySelector(".filter").textContent = oneCat.name;
+
+    //append it
+    document.querySelector("#buttonbox").appendChild(catCopy);
+
+}
+
+
+
+
 
 
 
