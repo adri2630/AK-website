7f479f1bb3a98339814e75aaf67f9b745ccfe38a
adri2630 <adri2630@stud.kea.dk>
2020-11-18 16:50:52 +0100

    finished

--- script.js
+++ script.js
@@ -3,29 +3,34 @@
 //fetch the categories
 fetch("http://keep-it-simple.site/wp-json/wp/v2/categories")
 .then(function(res){
-    console.log(res)
+
     return res.json();
 })
 .then(function(datacat){
         console.log(datacat)
     receivedCat(datacat);
+     fetchData();
 })
 
 
 function receivedCat(elements){
-    console.log(elements)
+
     elements.forEach(displayCategories)
 
 }
 // thi function will display one category find the template, clone it and fill it in and append it, what a multitasker
 function displayCategories(oneCat){
-        console.log(oneCat)
+
     //fidn the template
     const catTemplate = document.querySelector("#wordpresscategories").content;
 
     //clone said template
 const catCopy = catTemplate.cloneNode(true);
 // fill it with the stuffypu put in wordpress for better or worse why can i just put here lorem ipsum o sth ?
+
+
+
+
 
    catCopy.querySelector(".filter").textContent = oneCat.name;
 
@@ -38,34 +43,37 @@ const catCopy = catTemplate.cloneNode(true);
 
 
 
+function fetchData(){
+
 
 
 //fetch the data
 fetch("http://keep-it-simple.site/wp-json/wp/v2/product")
 .then(function(response){
-    console.log(response)
+
     return response.json();
 })
 .then(function(data){
-        console.log(data)
+
     receivedData(data);
 })
 
 function receivedData(products){
-    console.log(products)
+
     products.forEach(displayProducts)
 
 }
 // thi function will display one product find the template, clone it and fill it in and append it, what a multitasker
 function displayProducts(oneProduct){
-        console.log(oneProduct)
+
     //fidn the template
     const productTemplate = document.querySelector("#producttemplate").content;
 
     //clone said template
 const copy = productTemplate.cloneNode(true);
 // fill it with the stuffypu put in wordpress for better or worse why can i just put here lorem ipsum o sth ?
-
+///
+    ///
 
 
     copy.querySelector("h3").textContent = oneProduct.title.rendered;
@@ -77,28 +85,28 @@ const copy = productTemplate.cloneNode(true);
     document.querySelector("main").appendChild(copy);
 
 }
-
+}
 
 
 //fetch the data
 fetch("http://keep-it-simple.site/wp-json/wp/v2/contact/")
 .then(function(resp){
-    console.log(resp)
+
     return resp.json();
 })
 .then(function(datay){
-        console.log(datay)
+
     receivedContact(datay);
 })
 
 function receivedContact(cont){
-    console.log(cont)
+
     cont.forEach(displayContact)
 
 }
 // thi function will display one product find the template, clone it and fill it in and append it, what a multitasker
 function displayContact(oneContact){
-        console.log(oneContact)
+
     //fidn the template
     const contacttemp = document.querySelector("#contactTemp").content;
 
@@ -115,4 +123,7 @@ const copyT = contacttemp.cloneNode(true);
     document.querySelector(".insidecontact").appendChild(copyT);
 
 }
+/////
+
+
 
